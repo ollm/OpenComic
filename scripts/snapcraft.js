c4f80c190364f61f4ea2b1c39a981f08154c3e2f
Oleguer Llopart <oleguer.llopart.mora@gmail.com>
2019-07-11 11:57:56 +0200

    New: Add arm64 build commands

--- scripts/snapcraft.js
+++ scripts/snapcraft.js
@@ -5,8 +5,18 @@ var snapcraft = yaml.readSync('../dist/__snap-x64/snap/snapcraft.yaml');
 
 snapcraft.parts.unrar = {
 	'plugin': 'make',
-    'source': 'https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/unrar-nonfree/1:5.5.8-1/unrar-nonfree_5.5.8.orig.tar.gz',
-    'build-packages': ['g++'],
+	'source': 'https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/unrar-nonfree/1:5.5.8-1/unrar-nonfree_5.5.8.orig.tar.gz',
+	'build-packages': ['g++'],
 };
 
 yaml.writeSync('../dist/__snap-x64/snap/snapcraft.yaml', snapcraft)
+
+var snapcraftArm = yaml.readSync('../dist/__snap-arm64/snap/snapcraft.yaml');
+
+snapcraftArm.parts.unrar = {
+	'plugin': 'make',
+	'source': 'https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/unrar-nonfree/1:5.5.8-1/unrar-nonfree_5.5.8.orig.tar.gz',
+	'build-packages': ['g++'],
+};
+
+yaml.writeSync('../dist/__snap-arm64/snap/snapcraft.yaml', snapcraftArm)
