757dea9b47b2b65a85e0c9a48846b238d7332841
Dagdon <dagdonjephthah@gmail.com>
2025-05-29 14:17:08 +0100

    added MAL tracking keys

--- scripts/tracking/tracking-sites-keys.js
+++ scripts/tracking/tracking-sites-keys.js
@@ -3,9 +3,16 @@ authKeys = {
 	anilist: {
 		clientId: 3050,
 		clientSecret: '1ZUNqGak9QAHaGvaYjOgLuGXGCTpdTr18unz5koc',
-	}
+	},
+
+	// MAL keys
+	mal: {
+        clientId: 4bb3d90eaf4f31d6042e551db7f85ba6,
+        clientSecret: 72767e017847e38bbd4374306e394953c2c5691ef182be5f40121e85116f14f2
+    }
 };
 
 module.exports = {
 	authKeys: authKeys,
+	trackingSitesKeys,
 };
