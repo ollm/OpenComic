4e0a734d16e19abc967d3c0bb0d18e781cb2a43d
Dagdon <dagdonjephthah@gmail.com>
2025-05-30 16:28:34 +0100

    updated file

--- scripts/tracking/tracking-sites-keys.js
+++ scripts/tracking/tracking-sites-keys.js
@@ -1,13 +1,13 @@
 const authKeys = {
-    anilist: {
-        clientId: '3050',
-        clientSecret: '1ZUNqGak9QAHaGvaYjOgLuGXGCTpdTr18unz5koc',
-    },
-    mal: {
-        clientId: '4bb3d90eaf4f31d6042e551db7f85ba6',
-        clientSecret: '72767e017847e38bbd4374306e394953c2c5691ef182be5f40121e85116f14f2',
-        client_id: '4bb3d90eaf4f31d6042e551db7f85ba6',
-        client_secret: '72767e017847e38bbd4374306e394953c2c5691ef182be5f40121e85116f14f2'
-    }
+	anilist: {
+		clientId: '3050',
+		clientSecret: '1ZUNqGak9QAHaGvaYjOgLuGXGCTpdTr18unz5koc',
+	},
+	mal: {
+		clientId: '4bb3d90eaf4f31d6042e551db7f85ba6',
+		clientSecret: '72767e017847e38bbd4374306e394953c2c5691ef182be5f40121e85116f14f2',
+		client_id: '4bb3d90eaf4f31d6042e551db7f85ba6',
+		client_secret: '72767e017847e38bbd4374306e394953c2c5691ef182be5f40121e85116f14f2'
+	}
 };
 module.exports = authKeys;
